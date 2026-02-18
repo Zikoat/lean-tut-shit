d2992dd62883dcc72827f9737c114108fe1f35e1
Sigurd Schoeler <sschoeler99@gmail.com>
2026-02-18 18:35:01 +0100

    start with proof farmer

--- Main.lean
+++ Main.lean
@@ -2,3 +2,158 @@ import Shit
 
 def main : IO Unit :=
   IO.println s!"Hello, {hello}!"
+
+#eval 1 + 2
+#eval String.append "Hello, " "Lean!"
+
+#eval if 1 > 2 then "y" else "n"
+
+#eval String.append "yeetus" " feetus"
+#eval 42 + 19
+#eval String.append "A" (String.append "B" "C")
+#eval String.append (String.append "A" "B") "C"
+#check (1 - 2 : Int)
+
+def myName : String := "Sigurd"
+
+#eval String.append "hello " myName
+
+def add1 (n) := n + 1
+#eval add1 2
+def mymax (n : Nat) (k : Nat) := if n < k then k else n
+
+#eval mymax 1 2
+
+def joinStringWith (a : String) (b : String) (c : String) : String :=
+  String.append (String.append a b) c
+
+#eval joinStringWith "a" "b" "c"
+
+#check joinStringWith "a"
+
+#check 1.1
+
+structure MyPoint where
+  x : Float
+  y : Float
+
+def origin : MyPoint := { x := 0, y := 0 }
+#eval origin.x
+
+structure RectangularPrism where
+  height : Float
+  width : Float
+  depth : Float
+
+def volume (p : RectangularPrism) : Float :=
+  p.height * p.width * p.depth
+
+structure Point where
+  x : Float
+  y : Float
+
+structure Segment where
+  p1 : Point
+  p2 : Point
+
+def length (l : Segment) : Float :=
+  Float.sqrt ((Float.pow (l.p2.x - l.p1.x) 2) + (Float.pow (l.p2.y - l.p1.y) 2))
+
+#eval length { p1 := { x := 2, y := 4 }, p2 := { x := 6, y := 8 } }
+#check Segment.p1
+
+def plus (n : Nat) (k : Nat) : Nat :=
+  match k with
+  | Nat.zero => n
+  | Nat.succ k' => Nat.succ (plus n k')
+
+#check plus 3 2
+#reduce plus 3 2
+#eval plus 3 2
+
+set_option trace.Meta.Tactic.simp true in
+example : plus 3 2 = 5 := by
+  simp [plus]
+
+
+def times (n : Nat) (k : Nat) : Nat :=
+  match k with
+  | Nat.zero => Nat.zero
+  | Nat.succ k' => plus n (times n k')
+
+def minus (n : Nat) (k : Nat) : Nat :=
+  match k with
+  | Nat.zero => n
+  | Nat.succ k' => Nat.pred (minus n k')
+
+#eval Nat.pred 0
+#eval Nat.pred 2
+#eval minus 10 3
+#eval times 3 2
+
+-- def div (n : Nat) (k : Nat) : Nat :=
+--   if n < k then
+--     0
+--   else Nat.succ (div (n - k) k)
+
+#check  List Nat
+
+structure PPoint (α  : Type) where
+  x : α
+  y : α
+
+def natOrigin : PPoint Nat :=
+  { x := 3, y := 3 }
+
+#check natOrigin
+
+def noop: (_:Unit)->  Unit := fun b=>b
+def noop2 (_:Unit):Unit := ()
+def noop3 (_:Unit) := ()
+def noop4 : Unit → Unit :=
+  fun a => a
+
+#check noop
+#check noop2
+#check noop3
+#check noop4
+#eval noop ()
+
+partial def infiniteLoop (n:Nat) :Nat
+  := infiniteLoop n
+
+def nonZero (n:Nat):Nat :=
+  match n with
+  | Nat.zero => default
+  | Nat.succ k => Nat.succ k
+
+def last {α :Type} (xs:List α ):Option α :=
+match xs with
+| [] => none
+| y::List.nil=>some y
+| _::ys=>last ys
+
+#eval last [1,2,4]
+#check last
+
+def List.findFirst? {α :Type}
+  (xs:List α )
+  (predicate : α -> Bool)
+  : Option α :=
+match xs with
+| [] => none
+| y :: ys =>
+  if(predicate y) then y else List.findFirst? ys predicate
+
+
+def isNonZero(a:Nat):Bool :=
+match a with
+| Nat.zero => false
+|Nat.succ _ => true
+
+#eval List.findFirst? [0,0,0,2,3,0,] isNonZero
+
+def Prod.switch {α β : Type} (pair : α × β ): β × α :=
+(pair.snd, pair.fst)
+
+#eval Prod.switch ("hello", "world")
---
+++ ProofFarmer.lean
@@ -0,0 +1,174 @@
+def boolArray : Array Bool :=
+  #[false, false, false]
+
+-- #eval boolArray[0]
+-- #eval boolArray[3]?
+
+def toggleFirst (xs: List Bool):List Bool:=
+match xs with
+| y::ys => not y :: ys
+|[]=>[]
+
+-- #check []
+example:toggleFirst []=[]:=by rfl
+example:toggleFirst [false]=[true]:=by rfl
+example:toggleFirst [true]=[false]:=by rfl
+example:toggleFirst [true, true]=[false, true]:=by rfl
+
+def toggleNth2 (xs:List Bool) (n:Nat) : List Bool:=
+match n with
+| Nat.zero => toggleFirst xs
+| Nat.succ n' =>
+  match xs with
+  | [] => []
+  | y::ys=>y::toggleNth2 ys n'
+
+def toggleNth (xs:List Bool) (n:Nat) : List Bool:=
+match n, xs with
+| 0, [] => []
+| 0, y::ys => (not y) :: ys
+|Nat.succ _, [] => []
+|Nat.succ n',y::ys=>y::toggleNth ys n'
+
+-- #eval toggleNth [true, false, true] 1
+
+example:toggleNth [] 0 = [] := by rfl
+example:toggleNth [] 1 = [] := by rfl
+example:toggleNth [true] 0 = [false] := by rfl
+example:toggleNth [true] 1 = [true] := by rfl
+example:toggleNth [true, true] 1 = [true, false] := by rfl
+example:toggleNth [false, false, true] 1 = [false, true, true] := by rfl
+
+def toggleNthProperty (xs : List Bool) (n:Nat):Bool:=
+match xs[n]? with
+|none => true
+|some oldVal =>
+  match (toggleNth xs n)[n]? with
+  | some newVal => newVal != oldVal
+  | none => false
+
+-- #eval toggleNthProperty [true] 0
+-- #eval toggleNthProperty [] 0
+-- #eval toggleNthProperty [] 1
+-- #eval toggleNthProperty [false] 1
+-- #eval toggleNthProperty [false, true] 1
+-- #eval toggleNthProperty [false, true] 1
+
+def ToggleNthProp (xs : List Bool) (n:Nat):Prop:=
+toggleNthProperty xs n = true
+
+-- #check ToggleNthProp
+-- #check ToggleNthProp [true,false] 1
+example:ToggleNthProp [true,false] 1:=by {rfl}
+
+-- example:ToggleNthProp [false,false] 1:=by
+-- {
+--   sorry
+-- }
+-- example:2=2:=by sorry
+
+
+theorem get?_toggleNth
+  (xs:List Bool) (n:Nat) (b: Bool)
+  : xs[n]? = some b  -- assuming that n is in xs, call the element b
+  -> (toggleNth xs n)[n]? = some (not b) -- when we toggle it, it is the opposite of b
+  :=
+by{
+  intro h
+  induction n generalizing xs with
+  |zero =>
+    cases xs with
+    |nil=>
+      cases h
+    |cons y ys =>
+      cases h
+      rfl
+  |succ n' ih =>
+    cases xs with
+    |nil=>
+      cases h
+    |cons y ys=>
+      simp [toggleNth] at *
+      exact ih ys h
+}
+
+
+-- #check get?_toggleNth
+
+theorem toggleNth_length_same
+  (xs:List Bool) (n:Nat):
+  xs.length = (toggleNth xs n).length :=
+by {
+  induction n generalizing xs with
+  | zero =>
+    cases xs with
+    | nil =>
+      rfl
+    | cons y ys =>
+      rfl
+  | succ k ih =>
+    cases xs with
+    | nil=>
+      rfl
+    | cons y ys=>
+      simp [toggleNth]
+      exact ih ys
+}
+
+def setXIfCoordinateIsSame (x:Nat) (x': Nat) (a: String)  :  String :=
+  if x = x' then
+    "x"
+  else
+    a
+
+def drawBoard (board: List (List String)) (x:  Nat) (y: Nat) : String :=
+  String.intercalate "\n" ((
+      board.mapIdx fun y' row =>
+        if y' = (y) then
+          row.mapIdx (setXIfCoordinateIsSame x)
+        else
+          row
+    ).map (String.intercalate " "))
+
+def myMainProgram : IO Unit := do
+  let myPos := [2, 2]
+  let row :List String:= (List.replicate 5 ".")
+  let board: List (List String) :=
+    (List.replicate 5 row)
+
+  let boardString:String := drawBoard board myPos[0] myPos[1]
+
+  IO.println boardString
+
+def shit  (x: Nat) : List String := ((List.replicate 5 ".").mapIdx (fun idx val => (if idx = x then "x" else val)))
+
+def sleepTest :IO Unit:=do
+  let mut x := 0
+
+  while true do
+    IO.print "\r"
+    IO.print (String.intercalate " " (shit x))
+    (← IO.getStdout).flush
+    IO.sleep 200
+    -- todo it shouldnt be possible to end up outside of the board
+    x:= (x+1) % 6
+
+def main : IO Unit := do
+  IO.println "start"
+  sleepTest
+
+example : drawBoard [["."]] 0 0 = "x" := by rfl
+
+example : drawBoard [[".", "."], [".", "."]] 1 1 = ". .\n. x" := by rfl
+-- shit is it possible to show the expanded value, so it is easier to know what the calculation was?
+-- #eval  drawBoard [[".", "."], [".", "."]] 1 1
+
+-- todo this should not be possible
+-- #eval drawBoard [[".", "."], [".", "."]] 2 0
+-- todo this should not be possible
+-- #eval drawBoard [[".", "."], ["."]] 0 0
+
+-- todo we should refactor the x and y to be a {x,y} or a tuple with 2 elements or something
+
+-- todo THIS CAN BE RUN WITH
+-- cd /home/zikoat/dev/lean-tut/shit && lean --run ProofFarmer.lean
